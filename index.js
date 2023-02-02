179810deb63ed384c1163d59de4c8166daa5e851
Ansh Vijay <anshvijay28@gmail.com>
2023-02-02 11:37:02 -0500

    score increases by one when you win

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
- 
-
-function playRound(player) {
+ function playRound(player) {
 
     choices = ["Rock", "Paper", "Scissors"]; 
     const computerChoice = choices[Math.floor(Math.random() * 3)];
 
     const computerMessage = "The computer picked: " + computerChoice;
     const playerMessage = "You picked: " + player;
+
+    let messages = [computerMessage, playerMessage]
     
     if (computerChoice === "Rock") {
         if (player === "Rock") {
-            return [computerMessage, playerMessage, "It's a tie!"];
+            messages.push("It's a tie!", false);
         }
         else if (player === "Paper") {
-            return [computerMessage, playerMessage, "You win!"];
+            messages.push("You win!", true);
         }
         else {
-            return [computerMessage, playerMessage, "You lose!"]
+            messages.push("You lose!", false);
         }
     } 
     else if (computerChoice === "Paper") {
         if (player === "Rock") {
-            return [computerMessage, playerMessage, "You lose!"]
+            messages.push("You lose!", false);
         }
         else if (player === "Paper") {
-            return [computerMessage, playerMessage, "It's a tie!"]
+            messages.push("It's a tie!", false);
         }
         else {
-            return [computerMessage, playerMessage, "You win!"]
+            messages.push("You win!", true);
         }
     } 
     else {
         if (player === "Rock") {
-            return [computerMessage, playerMessage, "You win!"]
+            messages.push("You win!", true);
         }
         else if (player === "Paper") {
-            return [computerMessage, playerMessage, "You lose!"]
+            messages.push("You lose!", false);
         }
         else {
-            return [computerMessage, playerMessage, "It's a tie!"]
+            messages.push("It's a tie!", false);
         }
     }
+    return messages;
 }
 const userChoices = document.querySelectorAll('button');
 const gameMessage = document.querySelectorAll('#message-text, #player-text, #result-text');
+const score = document.querySelector('#score-text');
+
 userChoices.forEach(button => {
     button.addEventListener('click', e => {
         const messages = playRound(button.innerText);
-
         gameMessage[0].textContent = messages[0];
         gameMessage[1].textContent = messages[1];
         gameMessage[2].textContent = messages[2];
+        if (messages[3]) {
+            score.innerText++;
+        }
     })
 })
